36f8a303c522e59e72ff0021d3b992355189e646
Carita Angel Samudra Tjoatja <caritatjoatja@gmail.com>
2024-02-28 21:51:12 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide(n) {
+    showSlide(slideIndex += n);
+    console.log('nextSlide -> ' + n);
+    console.log('slideIndex -> ' + slideIndex);
+}
+
+function showSlide(n) {
+    let listImages = document.getElementById('header-img');
+    console.log('listImage -> ' + listImages.length);
+    console.log('n -> ' + n);
+    if(n > listImages.length) slideIndex = 1;
+    let i = 0;
+    while(i < listImages.length) {
+        listImages[i].style.display = 'none';
+        i++;
+    }
+    listImages[slideIndex - 1].style.display = 'block';
+    console.log(slideIndex);
+}
+
+const slideInterval = setInterval(() => nextSlide(1), 1000);
+
+document.getElementById('send-message-us-form').addEventListener('click', () => sendForm());
+
+function sendForm() {
+    let date = Date();
+    let name = document.forms['message-us-form']['name'];
+    let birth = document.forms['message-us-form']['birth'];
+    let gender = document.forms['message-us-form']['gender'];
+    let message = document.forms['message-us-form']['message'];
+    if(name.value == '' || birth.value == '' || gender.value == '' || message.value == '') {
+        alert('Harap Periksa Kembali Inputan Anda');
+    }
+    else{
+        document.getElementById('current-time').innerHTML = date;
+        document.getElementById('name'). innerHTML = name.value;
+        document.getElementById('birth'). innerHTML = birth.value;
+        document.getElementById('gender'). innerHTML = gender.value;
+        document.getElementById('message'). innerHTML = message.value;
+    }
+
+    return false;
+}
